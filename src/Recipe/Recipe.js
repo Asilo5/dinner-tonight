0336c1ab6504197e49ea07ef30c47230ba7eb919
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-05 13:13:48 -0600

    Add background colour

--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -6,7 +6,7 @@ const Recipe = ({title, publisher, image, recipeLink}) => {
         <section className='recipe'>
           <h3> {title} </h3>
           <img className='recipe-img' src={image} alt={title}/>
-          <p> Publisher: {publisher}</p>
+          <p> By: {publisher}</p>
           <button className='view-recipe-btn'><a href={recipeLink}>View Recipe</a></button>
         </section>
     )
