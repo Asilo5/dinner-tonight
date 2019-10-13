e6426b094b9579b48e5b4c00166552555596573d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-12 19:42:31 -0600

    Updates

---
+++ precache-manifest.eb2c586bccdc468032886df4e8e36ae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e248544820b6cc7f8058c0e262131f0",
+    "url": "/dinner-tonight/index.html"
+  },
+  {
+    "revision": "3235563bb623e763915d",
+    "url": "/dinner-tonight/static/css/main.cb6b9da0.chunk.css"
+  },
+  {
+    "revision": "a2593895e61c2dda9b05",
+    "url": "/dinner-tonight/static/js/2.73887d5b.chunk.js"
+  },
+  {
+    "revision": "3235563bb623e763915d",
+    "url": "/dinner-tonight/static/js/main.2b992f35.chunk.js"
+  },
+  {
+    "revision": "cc49ae23910b43090692",
+    "url": "/dinner-tonight/static/js/runtime-main.a52f1349.js"
+  }
+]);
